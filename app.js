f49a958006c746d45543e8d9d0f2142106760efc
Elvin Ahadov <elvineah@code.edu.az>
2024-09-05 16:32:08 +0400

    start

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("./"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT:${PORT}`);
+});
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("Database connection established.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
