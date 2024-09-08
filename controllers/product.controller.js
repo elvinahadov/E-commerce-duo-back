eaccb50a682bf9f72cd94d64f85ea4e5d9186f86
elvinahadov <elvineah@code.edu.az>
2024-09-09 00:08:54 +0400

    changes

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,7 +3,7 @@ import { Product } from "../models/product.models.js";
 const getProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).json(products);
+    res.status(200).json({message : "Product found",data:products});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -15,7 +15,7 @@ const getSingleProduct = async (req, res) => {
 
     if (!product) return res.status(404).json({ message: "Product not found" });
 
-    res.status(200).json(product);
+    res.status(200).json({message : "Product found",data:product});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
