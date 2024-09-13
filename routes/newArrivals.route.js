0bf311a8dc9eafb669ca559fe79cd1970902bcb7
Elvin Ahadov <elvineah@code.edu.az>
2024-09-13 16:15:17 +0400

    newandtop

--- routes/newArrivals.route.js
+++ routes/newArrivals.route.js
@@ -2,5 +2,5 @@ import express from "express";
 import { getNewArrivals } from "../controllers/newArrivals.controller";
 
 const router = express.Router();
-router.get("/getNewArrivals", getNewArrivals);
+router.get("/api/products/getNewArrivals", getNewArrivals);
 export default router;
--- routes/topSelling.route.js
+++ routes/topSelling.route.js
@@ -2,5 +2,5 @@ import express from "express";
 import { getTopSelling } from "../controllers/TopSelling.controller";
 
 const router = express.Router();
-router.get("/getTopSelling", getTopSelling);
+router.get("api/produts/getTopSelling", getTopSelling);
 export default router;
