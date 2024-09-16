43e3342830edd167d01eab5783113ce198626847
Farah Veyisli <ferehveyisli@gmail.com>
2024-09-16 15:49:08 +0400

    added

--- routes/newArrivals.route.js
+++ routes/newArrivals.route.js
@@ -2,5 +2,5 @@ import express from "express";
 import { getNewArrivals } from "../controllers/newArrivals.controller";
 
 const router = express.Router();
-router.get("/api/products/getNewArrivals", getNewArrivals);
+router.get(" ./getNewArrivals", getNewArrivals);
 export default router;
