c878f464405aab85ba7e299073025fca766ef6f0
Elvin Ahadov <elvineah@code.edu.az>
2024-09-16 15:47:29 +0400

    commit

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import productRouter from "./routes/product.route.js";
 import userRoute from "./routes/user.route.js";
+import newArrivalsRoute from "./routes/newArrivals.route.js";
+import topSellingRoute from "./routes/topSelling.route.js";
 import cors from "cors";
+
+dotenv.config();
 const app = express();
 app.use(cors());
-dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
 app.use(express.json());
@@ -17,17 +20,14 @@ app.use(cookieParser());
 app.use(express.static("./"));
 
 app.use("/api/products", productRouter);
+app.use("/api/newArrivals", newArrivalsRoute);
+app.use("/api/topSelling", topSellingRoute);
 app.use("/users", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT:${PORT}`);
 });
 
-mongoose
-  .connect(MONGODB_URL)
-  .then(() => {
-    console.log("Database connection established.");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(MONGODB_URL)
+  .then(() => console.log("Database connection established."))
+  .catch((err) => console.log(err));
--- controllers/topSelling.controller.js
+++ controllers/topSelling.controller.js
@@ -2,7 +2,7 @@ import {Product} from "../models/product.models.js";
 
 export const getTopSelling = async (req, res) => {
     try {
-      const products = await Product.find({TopSelling:true});
+      const products = await Product.find({topSelling:true});
       res.status(200).json({message : "Product found",data:products});
     } catch (error) {
       res.status(500).json({ message: error.message });
